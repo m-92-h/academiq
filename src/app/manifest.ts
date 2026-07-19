7342a4e143bba6339e6d49a4fffdfd5d92ad3f15
m-92-h <mhmt.code@gmail.com>
2026-07-19 10:56:05 +0300

    fix: image manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
     dir: "rtl",
     icons: [
       {
-        src: "/images/icon-192.png",
+        src: "/images/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "/images/icon-512.png",
+        src: "/images/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
