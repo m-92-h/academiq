9c0e9ee6cd9e6b3030acffede9d89fd78d777412
m-92-h <mhmt.code@gmail.com>
2026-07-19 15:18:09 +0300

    fix: progress

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,31 +1,18 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Progress as ProgressPrimitive } from "radix-ui"
+import * as React from "react";
+import { Progress as ProgressPrimitive } from "radix-ui";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Progress({
-  className,
-  value,
-  ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+function Progress({ className, value, dir, ...props }: React.ComponentProps<typeof ProgressPrimitive.Root> & { dir?: "rtl" | "ltr" }) {
+  const filled = value || 0;
+  const translate = dir === "rtl" ? `translateX(${100 - filled}%)` : `translateX(-${100 - filled}%)`;
   return (
-    <ProgressPrimitive.Root
-      data-slot="progress"
-      className={cn(
-        "relative flex h-1 w-full items-center overflow-x-hidden rounded-full bg-muted",
-        className
-      )}
-      {...props}
-    >
-      <ProgressPrimitive.Indicator
-        data-slot="progress-indicator"
-        className="size-full flex-1 bg-primary transition-all"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-      />
+    <ProgressPrimitive.Root data-slot="progress" dir={dir} className={cn("relative flex h-1 w-full items-center overflow-x-hidden rounded-full bg-muted", className)} {...props}>
+      <ProgressPrimitive.Indicator data-slot="progress-indicator" className="size-full flex-1 bg-primary transition-all" style={{ transform: translate }} />
     </ProgressPrimitive.Root>
-  )
+  );
 }
 
-export { Progress }
+export { Progress };
