04e43ce020b603fb4b21454055301d0ed0b4cd00
m-92-h <mhmt.code@gmail.com>
2026-07-19 11:04:26 +0300

    fix: icon manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -17,13 +17,25 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/images/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable",
+        purpose: "any",
       },
       {
         src: "/images/icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
+      },
+      {
+        src: "/images/icons/icon-maskable-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/images/icons/icon-maskable-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
